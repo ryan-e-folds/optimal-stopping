a0ec2fb90702ce21a7a13cd4908a1feeaecdd6ba
Ryan Folds <ryanfolds6@gmail.com>
2026-06-14 15:03:31 +0100

    chore: comments, verbosity

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from optimal_stopping import Selector
 
 
 def main():
-    s = Selector()
-    outcomes = [s.run() for _ in range(100)]
+    # create a selector to review 'candidates'
+    s = Selector(stopping=0.37)
+
+    # run N experiments
+    # reject the first 37% of candidates then choose next candidate that is better than the best yet
+    outcomes = [s.run() for _ in range(1000)]
+
+    # review results - how often did we get the best candidate?
     wins = len([outcome.is_best for outcome in outcomes if outcome.is_best]) / len(
         outcomes
     )
-    print(wins)
+    print(f"Win rate: {wins:.2%}")
 
 
 if __name__ == "__main__":
--- optimal_stopping.py
+++ optimal_stopping.py
@@ -20,22 +20,32 @@ def generate_values(n: int = 100) -> NumberSet:
 
 
 class Selector:
-    def __init__(self, stopping: float = 0.37):
+    def __init__(self, stopping: float = 0.37, verbose: bool = False):
         self.stopping = stopping
+        self.verbose = verbose
         self.reviewed = []
 
     def run(self, n: int = 100) -> Outcome:
         number_set = generate_values(n=n)
+        if self.verbose:
+            print(f"Best available: {number_set.best}.")
+
         stop_point = round(number=number_set.length * self.stopping)
         best_yet = max(number_set.numbers[: stop_point + 1])
+        if self.verbose:
+            print(f"Best after {stop_point}: {best_yet}.")
+
         for number in number_set.numbers[stop_point:]:
             if number > best_yet:
+                if self.verbose:
+                    print(f"Selecting: {number}")
                 return Outcome(
                     selected=number,
                     best=number_set.best,
                     is_best=number == number_set.best,
                 )
         else:
+            print(f"Selecting final number: {number}")
             return Outcome(
                 selected=number, best=number_set.best, is_best=number == number_set.best
             )
