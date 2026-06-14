f5f4f609eb4dc6d2404c41835e553d2eae5448d8
Ryan Folds <ryanfolds6@gmail.com>
2026-06-14 14:53:51 +0100

    feat: verified result

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from optimal_stopping import Selector
 
+
 def main():
     s = Selector()
-    outcome = s.run()
-    print(outcome)
+    outcomes = [s.run() for _ in range(100)]
+    wins = len([outcome.is_best for outcome in outcomes if outcome.is_best]) / len(
+        outcomes
+    )
+    print(wins)
 
 
 if __name__ == "__main__":
--- optimal_stopping.py
+++ optimal_stopping.py
@@ -1,25 +1,41 @@
-from multiprocessing.resource_sharer import stop
 import random
+from pydantic import BaseModel
 
-def generate_values(n: int = 100) -> list[float]:
-    return [random.random() for _ in range(n)]
+
+class NumberSet(BaseModel):
+    numbers: list[int]
+    best: int
+    length: int
+
+
+class Outcome(BaseModel):
+    selected: int
+    best: int
+    is_best: bool
+
+
+def generate_values(n: int = 100) -> NumberSet:
+    numbers = [random.randint(0, 1_000_000) for _ in range(n)]
+    return NumberSet(numbers=numbers, best=max(numbers), length=len(numbers))
 
 
 class Selector:
-
     def __init__(self, stopping: float = 0.37):
         self.stopping = stopping
         self.reviewed = []
 
-    def run(self):
-        numbers = generate_values()
-        print(f"{numbers=}")
-        stop_point = round(len(numbers) * self.stopping)
-        print(f"{stop_point=}")
-        best_yet = max(numbers[:stop_point + 1])
-        print(f"{best_yet=}")
-        for number in numbers[stop_point:]:
+    def run(self, n: int = 100) -> Outcome:
+        number_set = generate_values(n=n)
+        stop_point = round(number=number_set.length * self.stopping)
+        best_yet = max(number_set.numbers[: stop_point + 1])
+        for number in number_set.numbers[stop_point:]:
             if number > best_yet:
-                return number
+                return Outcome(
+                    selected=number,
+                    best=number_set.best,
+                    is_best=number == number_set.best,
+                )
         else:
-            return number
+            return Outcome(
+                selected=number, best=number_set.best, is_best=number == number_set.best
+            )
